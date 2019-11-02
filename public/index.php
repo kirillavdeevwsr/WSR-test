ab447125091b729b1034d5261736c243930e509e
n0zzy <ffnn83@yandex.ru>
2019-11-02 10:50:01 +0500

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,30 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="css/bootstrap-reboot.min.css">
+</head>
+<body>
+    <div class="site">
+        <div class="header">
+            <a href="home.html">1</a>
+        </div>
+        <div class="content">
+
+        </div>
+        <div class="basement">
+
+        </div>
+    </div>
+</body>
+<script src="js/jquery-3.4.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+</html>
+
